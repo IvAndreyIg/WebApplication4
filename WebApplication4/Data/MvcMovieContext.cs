6073eee4718e855d3cffb968ba3144497c575dc9
IvAndreyIg <frostpunchofpain@gmail.com>
2020-03-12 23:28:30 -0500

    Добавьте файлы проекта.

---
+++ WebApplication4/Data/MvcMovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+        /*public MvcMovieContext()
+            : base()
+        {
+        }*/
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
